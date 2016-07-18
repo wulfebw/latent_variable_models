f1aa8f413a678db8e171a679f02f1c569f0a698c
blake wulfe <wulfebw@DN0a229671.SUNet>
2016-07-18 15:31:25 -0700

    before log switch

--- 1_hidden_markov_model/tests/test_hmm.py
+++ 1_hidden_markov_model/tests/test_hmm.py
@@ -14,7 +14,7 @@ import hmm
 class TestHMM(unittest.TestCase):
 
     def test_forward(self):
-        data = np.array([[1,6]])
+        data = np.array([1,10])
         k = 2
         max_iterations = 10
         threshold = 1e-5
@@ -22,14 +22,13 @@ class TestHMM(unittest.TestCase):
         m.initialize()
         m.pi = np.array([.5,.5])
         m.A = np.array([[.5,.5],[.5,.5]])
-        m.B = np.array([1, 5])
+        m.B = np.array([1, 10])
         m.forward()
-        # check that the second class if more likely at the end
-        final_class_probs = (m.alphas[:,-1:,:] / np.sum(m.alphas[:,-1:,:]))[0][0]
-        self.assertTrue(final_class_probs[0] < final_class_probs[1])
+        # check that the second class is more likely at the end
+        self.assertTrue(m.alphas[-1,0] < m.alphas[-1,1])
 
         # multiple data case
-        data = np.array([[1,6], [7,1]])
+        data = np.array([1,6,7,1])
         k = 2
         max_iterations = 10
         threshold = 1e-5
@@ -39,16 +38,11 @@ class TestHMM(unittest.TestCase):
         m.A = np.array([[.5,.5],[.5,.5]])
         m.B = np.array([1, 5])
         m.forward()
-        # check that the second class if more likely at the end
-        final_class_probs = (m.alphas[:,-1:,:] / np.sum(m.alphas[:,-1:,:]))
-        probs_1 = final_class_probs[0][0]
-        probs_2 = final_class_probs[1][0]
-
-        self.assertTrue(probs_1[0] < probs_1[1])
-        self.assertTrue(probs_2[0] > probs_2[1])
+        # check that the first class is more likely at the end
+        self.assertTrue(m.alphas[-1,0] > m.alphas[-1,1])
 
     def test_backward(self):
-        data = np.array([[1, 1, 1, 1]])
+        data = np.array([10, 1, 1, 1])
         k = 2
         max_iterations = 10
         threshold = 1e-5
@@ -58,10 +52,9 @@ class TestHMM(unittest.TestCase):
         m.A = np.array([[1,0],[0,1]])
         m.B = np.array([1, 10])
         m.backward()
-        init_probs = m.betas[0][0]
-        self.assertTrue(init_probs[0] > init_probs[1])
+        self.assertTrue(m.betas[0,0] > m.betas[0,1])
 
-        data = np.array([[1,1,1,1], [5,5,5,5]])
+        data = np.array([1,1,1,1,5,5,5,5])
         k = 2
         max_iterations = 10
         threshold = 1e-5
@@ -71,27 +64,10 @@ class TestHMM(unittest.TestCase):
         m.A = np.array([[1,0],[0,1]])
         m.B = np.array([1, 5])
         m.backward()
-        init_probs_1 = m.betas[0][0]
-        self.assertTrue(init_probs_1[0] > init_probs_1[1])
-        init_probs_2 = m.betas[1][0]
-        self.assertTrue(init_probs_2[0] < init_probs_2[1])
+        self.assertTrue(m.betas[0, 0] < m.betas[0, 1])
 
     def test_e_step(self):
-        data = np.array([[1]])
-        k = 2
-        max_iterations = 10
-        threshold = 1e-5
-        m = hmm.HMM(data, k, max_iterations, threshold)
-        m.initialize()
-        m.pi = np.array([.5,.5])
-        m.A = np.array([[.9,.1],[.1,.9]])
-        m.B = np.array([1, 5])
-        m.e_step()
-        print m.gammas
-        # what even is gamma?
-
-    def test_m_step(self):
-        data = np.array([[1,5], [5,1], [1,1]])
+        data = np.array([1,2])
         k = 2
         max_iterations = 10
         threshold = 1e-5
@@ -100,7 +76,22 @@ class TestHMM(unittest.TestCase):
         m.pi = np.array([.5,.5])
         m.A = np.array([[.5,.5],[.5,.5]])
         m.B = np.array([1, 5])
-        for _ in range(100):
+        m.e_step()
+        print m.gammas
+        # what even is gamma?
+
+    def test_m_step(self):
+        # data = np.array([[1,5], [5,1], [1,1], [5,5]])
+        data = np.array([1,10,1,10])
+        k = 2
+        max_iterations = 10
+        threshold = 1e-5
+        m = hmm.HMM(data, k, max_iterations, threshold)
+        m.initialize()
+        m.pi = np.array([.5,.5])
+        m.A = np.array([[.5,.5],[.5,.5]])
+        m.B = np.array([1, 10])
+        for _ in range(10):
             m.e_step()
             m.m_step()
         print m.A
@@ -109,13 +100,12 @@ class TestHMM(unittest.TestCase):
         # what even is gamma?
 
     def test_hmm_on_generated_data(self):
-        A = np.array([[1,0],[0,1]])
-        B = np.array([[1],[10]])
+        A = np.array([[.5,.5],[.5,.5]])
+        B = np.array([[1],[3]])
         pi = np.array([.5,.5])
-        T = 2
+        T = 100
         dist = np.random.poisson
-        num_samples = 10
-        data = generate_data.generate_data(A, B, pi, T, dist, num_samples)
+        data = generate_data.generate_data(A, B, pi, T, dist)
         print data
 
         k = 2
